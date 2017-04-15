5baf9ee97ffb0fe179f0788757d9f5a49f9cdc53
BlackMesa123 <brother12@hotmail.it>
2017-04-15 13:00:54 +0200

    kernel: Fixed build errors

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=/home/blackmesa/Scrivania/Android/Sorgenti/Toolchain/google-arm-linux-androideabi-4.9/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export LOCALVERSION=-Espheni_Kernel_v1
 
 mkdir output
 
 make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -j64 -C $(pwd) O=output
+
+make -j14 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
