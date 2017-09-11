dc1826b6789aaa5579fb2549f09c14b14a35503d
BlackMesa <brother12@hotmail.it>
2017-09-11 12:03:47 +0200

    build: Remode build script

--- build_espheni.sh
+++ build_espheni.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Coded by BlackMesa @EspheniKernel
+# Coded by BlackMesa @EspheniKernel v2
 
 clear
 EK_VERSION=v2
 EK_DATE=$(date +%Y%m%d)
 echo "------------------------------------------"
-echo "EspheniKernel Build Script"
+echo "EspheniKernel v2 Build Script"
 echo "Coded by BlackMesa"
 echo "------------------------------------------"
-PS3='Please select the kernel variant you want to build: '
+PS3='Please select the kernel variant you want to build (1-10): '
 options=("klte" "kltechn" "kltechnduos" "kltedcm" "kltedd" "klteduos" "kltekdi" "kltekor" "kltespr" "kltevzw" "Exit")
 select opt in "${options[@]}"
 do
@@ -19,91 +19,103 @@ do
             echo "Building kernel for klte..."
             echo "------------------------------------------"
             echo " "
-			EK_VARIANT=klte
+            EK_VARIANT=klte
             export ARCH=arm
             export CROSS_COMPILE=/home/blackmesa/Scrivania/Android/Sorgenti/Toolchain/hyper-arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
             export LOCALVERSION=-Espheni_Kernel-$EK_VERSION-$EK_VARIANT-$EK_DATE
-			rm -r -f output
-			mkdir output
-			make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_klte_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
-			make -j64 -C $(pwd) O=output
-			cp output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+            make clean
+            rm -r -f output
+            mkdir output
+            make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_klte_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
+            make -j64 -C $(pwd) O=output
+            mv output/arch/arm/boot/zImage $(pwd)/ramdisk/$EK_VARIANT/split_img/boot.img-zImage
+            ./ramdisk/$EK_VARIANT/repackimg.sh
+            rm -f ramdisk/$EK_VARIANT/ramdisk-new.cpio.gz
             echo " "
             echo "------------------------------------------"
             echo "Kernel build finished."
-            echo "Image is located in /arch/arm/boot/zImage."
+            echo "boot.img is located into the ramdisk folder of the variant you selected."
             echo "Press any key for end the script."
             echo "------------------------------------------"
             read -n1 -r key
             break
-			;;
+            ;;
         "kltechn")
             clear
             echo "------------------------------------------"
             echo "Building kernel for kltechn..."
             echo "------------------------------------------"
             echo " "
-			EK_VARIANT=kltechn
+            EK_VARIANT=kltechn
             export ARCH=arm
             export CROSS_COMPILE=/home/blackmesa/Scrivania/Android/Sorgenti/Toolchain/hyper-arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
             export LOCALVERSION=-Espheni_Kernel-$EK_VERSION-$EK_VARIANT-$EK_DATE
-			rm -r -f output
-			mkdir output
-			make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltechn_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
-			make -C $(pwd) O=output
-			cp output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+            make clean
+            rm -r -f output
+            mkdir output
+            make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltechn_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
+            make -C $(pwd) O=output
+            mv output/arch/arm/boot/zImage $(pwd)/ramdisk/$EK_VARIANT/split_img/boot.img-zImage
+            ./ramdisk/$EK_VARIANT/repackimg.sh
+            rm -f ramdisk/$EK_VARIANT/ramdisk-new.cpio.gz
             echo " "
             echo "------------------------------------------"
             echo "Kernel build finished."
-            echo "Image is located in /arch/arm/boot/zImage."
+            echo "boot.img is located into the ramdisk folder of the variant you selected."
             echo "Press any key for end the script."
             echo "------------------------------------------"
             read -n1 -r key
             break
-			;;
+            ;;
         "kltechnduos")
             clear
             echo "------------------------------------------"
             echo "Building kernel for kltechnduos..."
             echo "------------------------------------------"
             echo " "
-			EK_VARIANT=kltechnduos
+            EK_VARIANT=kltechnduos
             export ARCH=arm
             export CROSS_COMPILE=/home/blackmesa/Scrivania/Android/Sorgenti/Toolchain/hyper-arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
             export LOCALVERSION=-Espheni_Kernel-$EK_VERSION-$EK_VARIANT-$EK_DATE
-			rm -r -f output
-			mkdir output
-			make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltechnduos_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
-			make -C $(pwd) O=output
-			cp output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+            make clean
+            rm -r -f output
+            mkdir output
+            make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltechnduos_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
+            make -C $(pwd) O=output
+            mv output/arch/arm/boot/zImage $(pwd)/ramdisk/$EK_VARIANT/split_img/boot.img-zImage
+            ./ramdisk/$EK_VARIANT/repackimg.sh
+            rm -f ramdisk/$EK_VARIANT/ramdisk-new.cpio.gz
             echo " "
             echo "------------------------------------------"
             echo "Kernel build finished."
-            echo "Image is located in /arch/arm/boot/zImage."
+            echo "boot.img is located into the ramdisk folder of the variant you selected."
             echo "Press any key for end the script."
             echo "------------------------------------------"
             read -n1 -r key
             break
-			;;
+            ;;
         "kltedcm")
             clear
             echo "------------------------------------------"
             echo "Building kernel for kltedcm..."
             echo "------------------------------------------"
             echo " "
-			EK_VARIANT=kltedcm
+            EK_VARIANT=kltedcm
             export ARCH=arm
             export CROSS_COMPILE=/home/blackmesa/Scrivania/Android/Sorgenti/Toolchain/hyper-arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
             export LOCALVERSION=-Espheni_Kernel-$EK_VERSION-$EK_VARIANT-$EK_DATE
-			rm -r -f output
-			mkdir output
-			make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltedcm_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
-			make -j64 -C $(pwd) O=output
-			cp output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+            make clean
+            rm -r -f output
+            mkdir output
+            make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltedcm_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
+            make -j64 -C $(pwd) O=output
+            mv output/arch/arm/boot/zImage $(pwd)/ramdisk/$EK_VARIANT/split_img/boot.img-zImage
+            ./ramdisk/$EK_VARIANT/repackimg.sh
+            rm -f ramdisk/$EK_VARIANT/ramdisk-new.cpio.gz
             echo " "
             echo "------------------------------------------"
             echo "Kernel build finished."
-            echo "Image is located in /arch/arm/boot/zImage."
+            echo "boot.img is located into the ramdisk folder of the variant you selected."
             echo "Press any key for end the script."
             echo "------------------------------------------"
             read -n1 -r key
@@ -115,19 +127,22 @@ do
             echo "Building kernel for kltedd..."
             echo "------------------------------------------"
             echo " "
-			EK_VARIANT=kltedd
+            EK_VARIANT=kltedd
             export ARCH=arm
             export CROSS_COMPILE=/home/blackmesa/Scrivania/Android/Sorgenti/Toolchain/hyper-arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
             export LOCALVERSION=-Espheni_Kernel-$EK_VERSION-$EK_VARIANT-$EK_DATE
-			rm -r -f output
-			mkdir output
-			make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltedd_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
-			make -C $(pwd) O=output
-			cp output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+            make clean
+            rm -r -f output
+            mkdir output
+            make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltedd_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
+            make -C $(pwd) O=output
+            mv output/arch/arm/boot/zImage $(pwd)/ramdisk/$EK_VARIANT/split_img/boot.img-zImage
+            ./ramdisk/$EK_VARIANT/repackimg.sh
+            rm -f ramdisk/$EK_VARIANT/ramdisk-new.cpio.gz
             echo " "
             echo "------------------------------------------"
             echo "Kernel build finished."
-            echo "Image is located in /arch/arm/boot/zImage."
+            echo "boot.img is located into the ramdisk folder of the variant you selected."
             echo "Press any key for end the script."
             echo "------------------------------------------"
             read -n1 -r key
@@ -139,19 +154,22 @@ do
             echo "Building kernel for klteduos..."
             echo "------------------------------------------"
             echo " "
-			EK_VARIANT=klteduos
+            EK_VARIANT=klteduos
             export ARCH=arm
             export CROSS_COMPILE=/home/blackmesa/Scrivania/Android/Sorgenti/Toolchain/hyper-arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
             export LOCALVERSION=-Espheni_Kernel-$EK_VERSION-$EK_VARIANT-$EK_DATE
-			rm -r -f output
-			mkdir output
-			make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_klteduos_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
-			make -C $(pwd) O=output
-			cp output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+            make clean
+            rm -r -f output
+            mkdir output
+            make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_klteduos_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
+            make -C $(pwd) O=output
+            mv output/arch/arm/boot/zImage $(pwd)/ramdisk/$EK_VARIANT/split_img/boot.img-zImage
+            ./ramdisk/$EK_VARIANT/repackimg.sh
+            rm -f ramdisk/$EK_VARIANT/ramdisk-new.cpio.gz
             echo " "
             echo "------------------------------------------"
             echo "Kernel build finished."
-            echo "Image is located in /arch/arm/boot/zImage."
+            echo "boot.img is located into the ramdisk folder of the variant you selected."
             echo "Press any key for end the script."
             echo "------------------------------------------"
             read -n1 -r key
@@ -163,19 +181,22 @@ do
             echo "Building kernel for kltekdi..."
             echo "------------------------------------------"
             echo " "
-			EK_VARIANT=kltekdi
+            EK_VARIANT=kltekdi
             export ARCH=arm
             export CROSS_COMPILE=/home/blackmesa/Scrivania/Android/Sorgenti/Toolchain/hyper-arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
             export LOCALVERSION=-Espheni_Kernel-$EK_VERSION-$EK_VARIANT-$EK_DATE
-			rm -r -f output
-			mkdir output
-			make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltekdi_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
-			make -j64 -C $(pwd) O=output
-			cp output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+            make clean
+            rm -r -f output
+            mkdir output
+            make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltekdi_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
+            make -j64 -C $(pwd) O=output
+            mv output/arch/arm/boot/zImage $(pwd)/ramdisk/$EK_VARIANT/split_img/boot.img-zImage
+            ./ramdisk/$EK_VARIANT/repackimg.sh
+            rm -f ramdisk/$EK_VARIANT/ramdisk-new.cpio.gz
             echo " "
             echo "------------------------------------------"
             echo "Kernel build finished."
-            echo "Image is located in /arch/arm/boot/zImage."
+            echo "boot.img is located into the ramdisk folder of the variant you selected."
             echo "Press any key for end the script."
             echo "------------------------------------------"
             read -n1 -r key
@@ -187,19 +208,22 @@ do
             echo "Building kernel for kltekor..."
             echo "------------------------------------------"
             echo " "
-			EK_VARIANT=kltekor
+            EK_VARIANT=kltekor
             export ARCH=arm
             export CROSS_COMPILE=/home/blackmesa/Scrivania/Android/Sorgenti/Toolchain/hyper-arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
             export LOCALVERSION=-Espheni_Kernel-$EK_VERSION-$EK_VARIANT-$EK_DATE
-			rm -r -f output
-			mkdir output
-			make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltekor_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
-			make -C $(pwd) O=output
-			cp output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+            make clean
+            rm -r -f output
+            mkdir output
+            make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltekor_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
+            make -C $(pwd) O=output
+            mv output/arch/arm/boot/zImage $(pwd)/ramdisk/$EK_VARIANT/split_img/boot.img-zImage
+            ./ramdisk/$EK_VARIANT/repackimg.sh
+            rm -f ramdisk/$EK_VARIANT/ramdisk-new.cpio.gz
             echo " "
             echo "------------------------------------------"
             echo "Kernel build finished."
-            echo "Image is located in /arch/arm/boot/zImage."
+            echo "boot.img is located into the ramdisk folder of the variant you selected."
             echo "Press any key for end the script."
             echo "------------------------------------------"
             read -n1 -r key
@@ -211,19 +235,22 @@ do
             echo "Building kernel for kltespr..."
             echo "------------------------------------------"
             echo " "
-			EK_VARIANT=kltespr
+            EK_VARIANT=kltespr
             export ARCH=arm
             export CROSS_COMPILE=/home/blackmesa/Scrivania/Android/Sorgenti/Toolchain/hyper-arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
             export LOCALVERSION=-Espheni_Kernel-$EK_VERSION-$EK_VARIANT-$EK_DATE
-			rm -r -f output
-			mkdir output
-			make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltespr_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
-			make -j64 -C $(pwd) O=output
-			cp output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+            make clean
+            rm -r -f output
+            mkdir output
+            make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltespr_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
+            make -j64 -C $(pwd) O=output
+            mv output/arch/arm/boot/zImage $(pwd)/ramdisk/$EK_VARIANT/split_img/boot.img-zImage
+            ./ramdisk/$EK_VARIANT/repackimg.sh
+            rm -f ramdisk/$EK_VARIANT/ramdisk-new.cpio.gz
             echo " "
             echo "------------------------------------------"
             echo "Kernel build finished."
-            echo "Image is located in /arch/arm/boot/zImage."
+            echo "boot.img is located into the ramdisk folder of the variant you selected."
             echo "Press any key for end the script."
             echo "------------------------------------------"
             read -n1 -r key
@@ -235,24 +262,27 @@ do
             echo "Building kernel for kltevzw..."
             echo "------------------------------------------"
             echo " "
-			EK_VARIANT=kltevzw
+            EK_VARIANT=kltevzw
             export ARCH=arm
             export CROSS_COMPILE=/home/blackmesa/Scrivania/Android/Sorgenti/Toolchain/hyper-arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
             export LOCALVERSION=-Espheni_Kernel-$EK_VERSION-$EK_VARIANT-$EK_DATE
-			rm -r -f output
-			mkdir output
-			make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltevzw_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
-			make -C $(pwd) O=output
-			cp output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+            make clean
+            rm -r -f output
+            mkdir output
+            make -C $(pwd) O=output espheni_msm8974_defconfig VARIANT_DEFCONFIG=espheni_msm8974pro_kltevzw_defconfig SELINUX_DEFCONFIG=espheni_selinux_defconfig
+            make -C $(pwd) O=output
+            mv output/arch/arm/boot/zImage $(pwd)/ramdisk/$EK_VARIANT/split_img/boot.img-zImage
+            ./ramdisk/$EK_VARIANT/repackimg.sh
+            rm -f ramdisk/$EK_VARIANT/ramdisk-new.cpio.gz
             echo " "
             echo "------------------------------------------"
             echo "Kernel build finished."
-            echo "Image is located in /arch/arm/boot/zImage."
+            echo "boot.img is located into the ramdisk folder of the variant you selected."
             echo "Press any key for end the script."
             echo "------------------------------------------"
             read -n1 -r key
             break
-			;;
+            ;;
         "Exit")
             break
             ;;
